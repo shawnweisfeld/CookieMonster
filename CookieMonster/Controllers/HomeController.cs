fea4be45a82be8a8aab7f35e2a0c8d3b514b8f37
shawnweisfeld <shawn@shawnweisfeld.com>
2014-08-15 13:03:11 -0500

    init

---
+++ CookieMonster/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieMonster.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //get the cookies
+            return View();
+        }
+
+        public ActionResult AddCookies()
+        {
+            //set the cookeis
+            var str = new RandomStringGenerator.StringGenerator();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Response.Cookies.Add(new HttpCookie(string.Format("mycookie-{1}-{0:yyyyMMdd-hhmmdd-ffff}", DateTime.Now, i), str.GenerateString(1024)));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CookieMonster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CookieMonster.Startup))]
+namespace CookieMonster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
